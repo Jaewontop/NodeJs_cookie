4a4f3514cb57d727c687e7362b39d9a34a2c3b1e
0912078 <0912078@naver.com>
2023-01-15 19:10:42 +0900

    Feat: permanent 쿠키 추가

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,9 +1,21 @@
 const http = require("http");
+var cookie = require("cookie");
 http
   .createServer(function (req, res) {
+    var cookies = {};
+    if (req.headers.cookies != undefined) {
+      cookies = cookie.parser(req.headers.cookies);
+      console.log(cookies.yummy_cookie);
+    } //쿠키를 서버에서 쏘고 왜 그걸 읽음?? 어떻게 읽 히는 거지?
+
     res.writeHead(200, {
-      "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"],
+      "Set-Cookie": [
+        "yummy_cookie=choco",
+        "tasty_cookie=strawberry",
+        `Permanent=cookies; Max-age=${60 * 60 * 24 * 30}`,
+      ],
     });
+
     res.end("cookie!!");
   })
   .listen(3000);
